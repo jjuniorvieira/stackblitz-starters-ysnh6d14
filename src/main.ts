b3d2eec3c6eb9ebf3e1f7f3f478e64e6567bf04e
jjuniorvieira <junior.jaava@gmail.com>
2026-05-23 15:17:48 +0200

    three signals created

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,61 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common'
 import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-    <button (click)="counter.set(counter() - 1)">--</button>
-    <span> Counter: {{ counter() }} </span>
-    <button (click)="counter.set(counter() + 1)">++</button>
+    <h1> Shopping card </h1>
+
+
+    <select
+      [ngModel]="quantity()"
+      (ngModelChange)="onQuantitySelected($event)"
+    >
+      <option disabled value="">--Select a quantity--</option>
+    
+      @for (q of qtyAvailable(); track q) {
+        <option [value]="q">
+          {{ q }}
+        </option>
+      }
+    </select>
+
+    <div> {{quantity()}} </div>
+    <div> {{selectedProduct().name}} </div>
+    <div> {{selectedProduct().price}} </div>
+    
   `,
 })
 export class App {
   name = 'Angular';
-  counter = signal(0);
+
+  quantity = signal(5);
+  qtyAvailable = signal([1,2,3,4,5,6]);
+  selectedProduct = signal<Product>({
+    id: 5,
+    name: 'hammer',
+    price: 12
+  }) 
+
+
+  constructor() {
+    console.log('In constructor: ', this.quantity());
+  }
+
+  onQuantitySelected(qtd: number){
+
+  }
+}
+
+export interface Product {
+  id: number; 
+  name: string; 
+  price: number;
 }
 
 bootstrapApplication(App);
